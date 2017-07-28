7320d4da6984df9b18184f804248ccfb9bbc06b5
Mike Freeman <mifreem@microsoft.com>
2017-07-29 07:53:35 +1000

    ext script

---
+++ bash-ext.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get install -y git apache2 php7.0 libapache2-mod-php7.0 stress
+
+cd /var/www/html && git clone http://github.com/freebo/azure-metadata
+
+echo "* * * * * cd /var/www/html/azure-metadata && git pull http://github.com/freebo/azure-metadata ">>/var/spool/cron/crontabs/root
