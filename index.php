fb39cc9ae8bdd9af1f44200119bcd473c26c3586
Mike Freeman <mifreem@microsoft.com>
2017-07-29 07:28:00 +1000

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Based on alphamusk's AWS metadata page
+// Based on alphamusk's AWS metadata page ht
 // Enable better php debugging
 ini_set('display_errors', 'On');
 //error_reporting(E_ALL | E_STRICT);
