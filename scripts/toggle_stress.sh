2aec2865f34e1b78c0834917310f6ef7624525f7
Michael Freeman <mike@imac.home.mikejfreeman.com>
2017-07-29 15:53:31 +1000

    silence pgrep

--- scripts/toggle_stress.sh
+++ scripts/toggle_stress.sh
@@ -1,7 +1,7 @@
 #start stress process if not running, else stop stress process
 
 if  pgrep stress >/dev/null 2>&1; then
-	pkill stress ;
+	pkill stress >/dev/null 2>&1;
 	else
 		stress -c 1&
        	fi
