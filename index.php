8a4c179ef950fce1bfda13d67f9c7bda0c569557
Mike Freeman <freeman.mj@gmail.com>
2017-08-11 10:05:41 +1000

    depracate curl

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 // Credit
 $author_name = 'Mike Freeman';
-$author_version = 'v0.92b';
+$author_version = 'v0.92c';
 $author_email = 'freeman.mj@gmail.com';
 $author_project = 'Azure Metadata PHP Page';
 
@@ -15,7 +15,7 @@ $meta_host = '169.254.169.254';
 
 
 //$json_meta =  exec( $curl_cmd." \"http://".$meta_host."/metadata/instance?api-version=2017-04-02\"");
-$json_meta =  file_get_contents( $curl_cmd." \"http://".$meta_host."/metadata/instance?api-version=2017-04-02\"");
+$json_meta =  file_get_contents( "http://".$meta_host."/metadata/instance?api-version=2017-04-02\");
 $metad = json_decode($json_meta, true);
 
 
