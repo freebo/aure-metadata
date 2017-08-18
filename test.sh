c21e7c1adcd98b13a40140af22b26741c81cf851
Mike Freeman <freeman.mj@gmail.com>
2017-08-18 17:24:51 +1000

    script

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-apt-get update
+apt-get update && apt-get install -y git apache2 php7.0 libapache2-mod-php7.0 stress
+
+cd /var/www/html && git clone http://github.com/freebo/azure-metadata
+
+cronjob="*/2 * * * * cd /var/www/html && git pull https://github.com/freebo/azure-metadata.git > /dev/null 2>&1" (crontab -u root -l; echo "${cronjob}" ) | crontab -u ${root} - crontab -l
