6107ced9ae0aab3e6cbe806592cf019b608233b1
Mike Freeman <freeman.mj@gmail.com>
2017-08-19 11:49:47 +1000

    git clone www

--- test.sh
+++ test.sh
@@ -2,5 +2,8 @@
 
 apt-get update && apt-get install -y git apache2 php7.0 libapache2-mod-php7.0 stress
 
-cd /var/www/html && git clone http://github.com/freebo/azure-metadata
+rm -f /var/www/html/index.html
+
+cd /var/www/html && git clone http://github.com/freebo/azure-metadata .
+
 
